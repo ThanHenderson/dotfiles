a93ef32f093e8ef1e8fe2199fa81dbdcb64c2d9f
ThanHenderson <nathan.t.henderson@outlook.com>
2022-04-14 15:37:54 -0600

    Update.

--- ch.sh
+++ ch.sh
@@ -7,6 +7,5 @@ selection=`printf "$langs" | fzf`
 printf "Language: $selection\n"
 read -p "Search term: " term
 if printf "$langs" | grep -qs $selection; then
-    echo "heere"
     tmux neww bash -c "curl cht.sh/$selection/`echo $term | tr ' ' '+'` & while [ : ]; do sleep 1; done"
 fi
