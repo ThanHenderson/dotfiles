5effe78254137fc78ac6ef69f767d18ce3675440
Nathan Henderson <nathan.t.henderson@outlook.com>
2024-05-20 01:26:14 -0400

    enable gpg verification by default

--- bringup.sh
+++ bringup.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env sh
 
-git config --global user.name "ThanHenderson"
+git config --global user.name "Nathan Henderson"
 git config --global user.email "nathan.t.henderson@outlook.com"
+git config --global commit.gpgsign true
 
